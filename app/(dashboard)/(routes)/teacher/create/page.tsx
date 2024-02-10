c27f38ffe1a7d60b10324ac48635596b5c9bea61
Jheff4 <75406244+Jheff4@users.noreply.github.com>
2024-02-10 15:21:30 +0100

    added toast notification

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -4,7 +4,11 @@ import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import toast from "react-hot-toast";
+import Link from "next/link";
 
 import {
   Form,
@@ -16,8 +20,6 @@ import {
   FormMessage
 } from "@/components/ui/form";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 
 const formSchema = z.object({
   title: z.string().min(1, {
@@ -26,6 +28,8 @@ const formSchema = z.object({
 });
 
 const CreatePage = () => {
+  const router = useRouter();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -35,8 +39,13 @@ const CreatePage = () => {
 
   const { isSubmitting, isValid } = form.formState;
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values)
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const response = await axios.post("/api/course", values);
+      router.push(`/teacher/courses/${response.data.id}`);
+    } catch {
+      toast.error("Something went wrong")
+    }
   }
 
   return ( 
@@ -75,6 +84,22 @@ const CreatePage = () => {
                 </FormItem>
               )}
             />
+            <div className="flex items-center gap-x-2">
+              <Link href="/">
+                <Button
+                  type="button"
+                  variant="ghost"
+                >
+                  Cancel
+                </Button>
+              </Link>
+              <Button
+                type="submit"
+                disabled={!isValid || isSubmitting}
+              >
+                Continue
+              </Button>    
+            </div>
           </form>
         </Form>
       </div>
